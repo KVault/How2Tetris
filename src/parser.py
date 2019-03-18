4716fb6055f635b8952475ce3b5f8d7a8a8283d4
Álex Sáez <alexsaezm@gmail.com>
2019-03-18 23:32:33 +0100

    Create parser.py

---
+++ src/parser.py
@@ -0,0 +1,13 @@
+class Parser:
+    """List of utilities to parse a given frame and return the content in form of text"""
+
+    def __init__(self):
+        pass
+
+    def get_text(self, frame):
+        """return the text found in the frame"""
+        pass
+
+    def get_tetromino(self, frame):
+        """return the tetromino found in the frame"""
+        pass
